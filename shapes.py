c96a4094c0f16b8785de5595ccfd9898a861f643
bolucian <wambuilucian@gmail.com>
2019-05-02 19:25:47 +0300

    shapes.py

---
+++ shapes.py
@@ -0,0 +1,59 @@
+class Circle:
+	
+
+	def __init__(self,radius):
+		self.radius=radius
+		
+		Area_Circle=(22/7)*self.radius**self.radius
+		return Area_Circle
+
+		Circumference_Circle=2*(22/7)*self.radius
+		return Circumference_Circle
+	
+
+
+
+class Square:
+
+	def __init__(self, side_a):
+		self.side_a=side_a
+
+		Area_Square=self.side_a*self.side_a
+		return Area_Square
+
+		Perimeter_Square=4(self.side_a)
+		return Perimeter_Square
+
+
+
+
+class Rectangle:
+
+	def __init__(self,length,width):
+		self.length=length
+		self.width=width
+
+		Area_rectangle=self.length*self.width
+		return Area_rectangle
+
+		Perimeter_rectangle=2(self.length+self.width)
+		return Perimeter_rectangle
+
+
+
+
+class Sphere:
+
+	def __init__(self,radius_r):
+		self.radius_r=radius_r
+
+		Surface_area=4*(22/7)*self.radius_r*self.radius_r
+		return Surface_area
+
+		Volume=4/3*((22/7)*self.radius_r*self.radius_r*self.radius_r
+		return Volume
+
+
+
+
+		
